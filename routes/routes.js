bc8b71572356a9d96dc2aefc0ff8db382530174d
benib <beni.buess@nzz.ch>
2017-12-17 14:39:52 +0100

    add health route, unset license

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,6 @@ module.exports = [
   require('./rendering-info/web.js'),
   require('./stylesheet.js'),
   require('./schema.js'),
-  // require('./health.js'),
+  require('./health.js'),
   require('./fixtures/data.js')
 ];
