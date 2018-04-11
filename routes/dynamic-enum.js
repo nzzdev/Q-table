ba997f8a876731fae151a40adb51e885af634e09
Philip Küng <kueng.philip@gmail.com>
2018-04-11 16:34:24 +0200

    able to select a row to use as basis for showing a mini- bar in a new column

---
+++ routes/dynamic-enum.js
@@ -0,0 +1,37 @@
+const Boom = require("boom");
+const Joi = require("joi");
+
+function getMiniBarEnum(item) {
+  if (item.data.length < 1) {
+    return [null];
+  }
+  // constructs an array like [null,0,1,2,3,...] with as many indexes as there are data columns
+  return [null].concat([...item.data[0].slice(1).keys()]);
+}
+
+function getMiniBarEnumTitles(item) {
+  if (item.data.length < 1) {
+    return ["keine"];
+  }
+  return ["keine"].concat(item.data[0].slice(1));
+}
+
+module.exports = {
+  method: "POST",
+  path: "/dynamic-enum/{optionName}",
+  options: {
+    validate: {
+      payload: Joi.object()
+    },
+    cors: true
+  },
+  handler: function(request, h) {
+    if (request.params.optionName === "showColumnAsMiniBar") {
+      return {
+        enum: getMiniBarEnum(request.payload),
+        enum_titles: getMiniBarEnumTitles(request.payload)
+      };
+    }
+    return Boom.badRequest();
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ module.exports = [
   require("./rendering-info/web.js"),
   require("./stylesheet.js"),
   require("./option-availability.js"),
+  require("./dynamic-enum.js"),
   require("./health.js"),
   require("./fixtures/data.js")
 ].concat(require("./schema.js"));
