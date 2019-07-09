2a0ca5a5e3cf14ab51ad30b5a5637c6b7eb8e940
Philip Küng <kueng.philip@gmail.com>
2019-07-09 11:57:42 +0200

    replace dynamic-enum with dynamic-schema

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ module.exports = [
   require("./rendering-info/web.js"),
   require("./stylesheet.js"),
   require("./option-availability.js"),
-  require("./dynamic-enum.js"),
+  require("./dynamic-schema.js"),
   require("./health.js"),
   require("./migration.js"),
   require("./locales.js"),
