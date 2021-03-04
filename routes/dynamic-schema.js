e4349fef06ccada3f6b5c5182ac463349373d1b6
Philip Kueng <philip.kueng@nzz.ch>
2021-03-04 11:23:25 +0100

    dynamic schema refactor

--- routes/dynamic-schema.js
+++ routes/dynamic-schema.js
@@ -2,23 +2,40 @@ const Boom = require("@hapi/boom");
 const Joi = require("@hapi/joi");
 const getNumericColumns = require("../helpers/data.js").getNumericColumns;
 
-function getMiniBarEnum(item) {
+function getOptionEnum(item, type) {
   if (item.data.table.length < 1) {
     return [null];
   }
 
-  return [null].concat(
-    ...getNumericColumns(item.data.table).map(col => col.index)
-  );
+  if (type === "minibar") {
+    // todo: filter if heatmap is set 
+    return [null].concat(
+      ...getNumericColumns(item.data.table).map(col => col.index)
+    );
+  } else if (type === "heatmap") {
+    // todo: filter if minibar is set 
+    return [null].concat(
+      ...getNumericColumns(item.data.table).map(col => col.index)
+    );
+  }
 }
 
-function getMiniBarEnumTitles(item) {
+function getOptionEnumTitles(item, type) {
   if (item.data.table.length < 1) {
     return ["keine"];
   }
-  return ["keine"].concat(
-    ...getNumericColumns(item.data.table).map(col => col.title)
-  );
+
+  if (type === "minibar") {
+    // todo: filter if heatmap is set 
+    return ["keine"].concat(
+      ...getNumericColumns(item.data.table).map(col => col.title)
+    );
+  } else if (type === "heatmap") {
+    // todo: filter if minibar is set 
+    return ["keine"].concat(
+      ...getNumericColumns(item.data.table).map(col => col.title)
+    );
+  }
 }
 
 module.exports = {
@@ -30,22 +47,22 @@ module.exports = {
     },
     cors: true
   },
-  handler: function(request, h) {
+  handler: function (request, h) {
     const item = request.payload.item;
     if (request.params.optionName === "selectedColumnMinibar") {
       return {
-        enum: getMiniBarEnum(item),
+        enum: getOptionEnum(item, "minibar"),
         "Q:options": {
-          enum_titles: getMiniBarEnumTitles(item)
+          enum_titles: getOptionEnumTitles(item, "minibar")
         }
       };
     }
 
     if (request.params.optionName === "selectedColumnHeatmap") {
       return {
-        enum: getMiniBarEnum(item),
+        enum: getOptionEnum(item, "heatmap"),
         "Q:options": {
-          enum_titles: getMiniBarEnumTitles(item)
+          enum_titles: getOptionEnumTitles(item, "heatmap")
         }
       };
     }
