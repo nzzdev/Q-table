bc2695d44ea8d7436c8c3373760e5f08cebcc2d3
Franco Gervasi <franco.gervasi@nzz.ch>
2022-01-27 13:48:22 +0100

    Map light color font to colorClass

--- helpers/colorColumnColor.js
+++ helpers/colorColumnColor.js
@@ -1,3 +1,5 @@
+const colorClassWithLightFontList = require("./colorClassLightFont");
+
 const digitWords = [
   "one",
   "two",
@@ -13,55 +15,19 @@ const digitWords = [
   "twelve",
 ];
 
-function getBucketTextColor(customColor, colorClassData) {
-  if (customColor !== undefined && customColor.textColor !== undefined) {
-    return customColor.textColor === "light"
-      ? "s-color-gray-1"
-      : "s-color-gray-9";
-  } else {
-    if (colorClassData.scale === "sequential") {
-      if (["three"].includes(colorClassData.colorScheme)) {
-        return colorClassData.scalePosition === 1
-          ? "s-color-gray-1"
-          : "s-color-gray-9";
-      }
-      if (["one", "female"].includes(colorClassData.colorScheme)) {
-        if (colorClassData.scaleSize < 5) {
-          return colorClassData.scalePosition === 1
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        } else {
-          return colorClassData.scalePosition < 3
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        }
-      }
-    } else {
-      if (["one", "gender"].includes(colorClassData.colorScheme)) {
-        if (colorClassData.scaleSize < 8) {
-          return colorClassData.scalePosition === colorClassData.scaleSize
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        } else {
-          return colorClassData.scalePosition >= colorClassData.scaleSize - 1
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        }
-      }
-      if (["two"].includes(colorClassData.colorScheme)) {
-        if (colorClassData.scaleSize < 9) {
-          return colorClassData.scalePosition === 1
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        } else {
-          return colorClassData.scalePosition < 3
-            ? "s-color-gray-1"
-            : "s-color-gray-9";
-        }
-      }
-    }
-    return "s-color-gray-9";
+function getBucketTextColor(customColor, colorClass) {
+  if (
+    customColor !== undefined &&
+    customColor.textColor !== undefined &&
+    customColor.textColor === "light"
+  ) {
+    return "s-color-gray-1";
   }
+
+  if (colorClassWithLightFontList.indexOf(colorClass) > -1) {
+    return "s-color-gray-1";
+  }
+  return "s-color-gray-9";
 }
 
 function getCategoryTextColor(colorScheme, customColor) {
@@ -88,15 +54,10 @@ function getBucketColor(numberBuckets, index, scale, colorOptions) {
   let textColor = "";
 
   if (scale === "sequential") {
-    colorClass = `s-viz-color-sequential-${colorScheme}-${numberBuckets}-${numberBuckets - index
+    colorClass = `s-viz-color-sequential-${colorScheme}-${numberBuckets}-${
+      numberBuckets - index
     }`;
-
-    textColor = getBucketTextColor(customColor, {
-      scale,
-      colorScheme,
-      scaleSize: numberBuckets,
-      scalePosition: numberBuckets - index,
-    });
+    textColor = getBucketTextColor(customColor, colorClass);
   } else {
     // if we have a diverging scale we deal with two cases:
     // a) diverging value = one of bucket border values,
@@ -138,13 +99,7 @@ function getBucketColor(numberBuckets, index, scale, colorOptions) {
     }
 
     colorClass = `s-viz-color-diverging-${colorScheme}-${scaleSize}-${scalePosition}`;
-
-    textColor = getBucketTextColor(customColor, {
-      scale,
-      colorScheme,
-      scaleSize,
-      scalePosition,
-    });
+    textColor = getBucketTextColor(customColor, colorClass);
   }
   return {
     colorClass,
