e6622c975a64e994773c696e8234e4732ed2312e
Danijel Beljan <danijel.beljan@protonmail.com>
2022-06-16 17:57:27 +0200

    Clean up dist folder and compile styles build procedure

--- tasks/compileStyleFiles.cjs
+++ tasks/compileStyleFiles.cjs
@@ -1,4 +1,3 @@
-// These lines make "require" available
 const fs = require('fs');
 const crypto = require('crypto');
 
@@ -11,81 +10,92 @@ const cssnano = require('cssnano');
 const stylesDir = __dirname + '/../src/styles/';
 
 function writeHashmap(hashmapPath, files, fileext) {
-    const hashMap = {};
-    files
-        .map((file) => {
-            const hash = crypto.createHash('md5');
-            hash.update(file.content, { encoding: 'utf8' });
-            file.hash = hash.digest('hex');
-            return file;
-        })
-        .map((file) => {
-            hashMap[file.name] = `${file.name}.${file.hash.substring(0, 8)}.${fileext}`;
-        });
+  const hashMap = {};
 
-    fs.writeFileSync(hashmapPath, JSON.stringify(hashMap));
+  files
+    .map((file) => {
+      const hash = crypto.createHash('md5');
+      hash.update(file.content, { encoding: 'utf8' });
+      file.hash = hash.digest('hex');
+      return file;
+    })
+    .map((file) => {
+      hashMap[file.name] = `${file.name}.${file.hash.substring(0, 8)}.${fileext}`;
+    });
+
+  fs.writeFileSync(hashmapPath, JSON.stringify(hashMap));
 }
 
 async function compileStylesheet(name) {
-    return new Promise((resolve, reject) => {
-        const filePath = stylesDir + `${name}.scss`;
-        fs.exists(filePath, (exists) => {
-            if (!exists) {
-                reject(`stylesheet not found ${filePath}`);
+  return new Promise((resolve, reject) => {
+    const filePath = stylesDir + `${name}.scss`;
+
+    const exists = fs.existsSync(filePath);
+    if (!exists) {
+      reject(`stylesheet not found ${filePath}`);
+      process.exit(1);
+    }
+
+    sass.render(
+      {
+        file: filePath,
+        includePaths: ['jspm_packages/github/', 'jspm_packages/npm/'],
+        outputStyle: 'compressed',
+      },
+      (err, sassResult) => {
+        if (err) {
+          reject(err);
+        } else {
+          postcss()
+            .use(postcssImport)
+            .use(autoprefixer)
+            .use(cssnano)
+            .process(sassResult.css, {
+              from: `${stylesDir}${name}.css`,
+            })
+            .then((prefixedResult) => {
+              if (prefixedResult.warnings().length > 0) {
+                console.log(`failed to compile stylesheet ${name}`);
                 process.exit(1);
-            }
-            sass.render(
-                {
-                    file: filePath,
-                    includePaths: ['jspm_packages/github/', 'jspm_packages/npm/'],
-                    outputStyle: 'compressed',
-                },
-                (err, sassResult) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        postcss()
-                            .use(postcssImport)
-                            .use(autoprefixer)
-                            .use(cssnano)
-                            .process(sassResult.css, {
-                                from: `${stylesDir}${name}.css`,
-                            })
-                            .then((prefixedResult) => {
-                                if (prefixedResult.warnings().length > 0) {
-                                    console.log(`failed to compile stylesheet ${name}`);
-                                    process.exit(1);
-                                }
-                                resolve(prefixedResult.css);
-                            });
-                    }
-                }
-            );
-        });
-    });
+              }
+              resolve(prefixedResult.css);
+            });
+        }
+      }
+    );
+  });
 }
 
 async function buildStyles() {
-    // compile styles
-    const styleFiles = [
-        {
-            name: 'q-table',
-            content: await compileStylesheet('q-table'),
-        },
-    ];
+  const basePath = 'dist/styles';
+  const fullPathHashmap = `${basePath}/hashMap.json`;
 
-    styleFiles.map((file) => {
-        fs.writeFileSync(`dist/styles/${file.name}.css`, file.content, { flag: 'w' });
-    });
+  // Check if directory exists.
+  if (!fs.existsSync(basePath)) {
+    fs.mkdirSync(basePath);
+  }
 
-    writeHashmap('dist/styles/hashMap.json', styleFiles, 'css');
+  const styleFiles = [
+    {
+      name: 'q-table',
+      content: await compileStylesheet('q-table'),
+    },
+  ];
+
+  styleFiles.map(file => {
+    const fullPathCss = `${basePath}/${file.name}.css`;
+    fs.writeFileSync(fullPathCss, file.content, { flag: 'w' });
+  });
+
+  writeHashmap(fullPathHashmap, styleFiles, 'css');
 }
 
-Promise.all([buildStyles()])
-    .then((res) => {
-        console.log('build complete');
-    })
-    .catch((err) => {
-        console.log(err);
-        process.exit(1);
-    });
+Promise.all([
+  buildStyles(),
+]).then(() => {
+    console.log('build complete');
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
